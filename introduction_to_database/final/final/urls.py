83c9c34dbab4b60339c609371f613c737da4fac9
wuhaotian <thelongestmailaccounteverexist@gmail.com>
2013-12-28 11:46:49 +0800

    import views

--- introduction_to_database/final/final/urls.py
+++ introduction_to_database/final/final/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from views import TopView, HotView, NearbyView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'final.views.home', name='home'),
     url(r'^login', 'final.views.login', name='login'),
-    url(r'^eatings/top.json', 'final.views.top', name='top'),
+    url(r'^eatings/top.json', TopView.as_view(), name='top'),
+    url(r'^eatings/hot.json', 'final.views.hot', name='hot'),
+    url(r'^eatings/nearby.json', 'final.views.nearby', name='nearby'),
     # url(r'^final/', include('final.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
